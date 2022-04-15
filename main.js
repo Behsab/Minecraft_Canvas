0d07b4513794a36fd8ac49810387284ab04e33a8
Behsab <87884840+Behsab@users.noreply.github.com>
2022-04-15 15:52:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ block_object="";
 function player_update(){
     fabric.Image.fromURL("player.png",function(Img){
     player_object = Img;
-    player_object.scaletoWidth(150);
-    player_object.scaletoHeight(140);
+    player_object.scaleToWidth(150);
+    player_object.scaleToHeight(140);
     player_object.set({
         top:player_y,
         left:player_x
@@ -26,8 +26,8 @@ function player_update(){
 function new_image(get_image){
     fabric.Image.fromURL(get_image,function(Img){
         block_object = Img;
-        block_object.scaletoWidth(block_image_width);
-        block_object.scaletoHeight(block_image_height);
+        block_object.scaleToWidth(block_image_width);
+        block_object.scaleToHeight(block_image_height);
         block_object.set({
             top:player_y,
         left:player_x
@@ -37,7 +37,7 @@ function new_image(get_image){
 }
 
 
-window.addEventListener("keydown", mykeydown());
+window.addEventListener("keydown", mykeydown);
 
 function mykeydown(e){
     var keypressed =e.keyCode;
@@ -129,4 +129,36 @@ if(keypressed=='67'){
 }
 
 
+}
+function up(){
+    if(player_y>=0){
+        player_y=player_y-block_image_height;
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down(){
+    if(player_y<550){
+        player_y=player_y+block_image_height;
+        canvas.remove(player_object);
+        player_update();
+    }
+    
+}
+
+function left(){
+    if(player_x>=0){
+       player_x=player_x-block_image_width;
+       canvas.remove(player_object); 
+       player_update();
+    }
+}
+
+function right(){
+    if(player_x<950){
+        player_x=player_x+block_image_width;
+        canvas.remove(player_object);
+        player_update();
+    }
 }
